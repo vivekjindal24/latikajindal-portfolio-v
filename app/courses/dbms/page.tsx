e204fe736270bc7276ab25648961ea21cfcf2ac3
vivekjindal24 <vivek.jindal.sbg@gmail.com>
2026-01-18 01:09:38 +0530

    Fix syntax error: Remove duplicate closing tags in DBMS page.tsx

--- app/courses/dbms/page.tsx
+++ app/courses/dbms/page.tsx
@@ -111,13 +111,6 @@ export default function Home() {
     </div>
   );
 }
-                  </div>
-                </div>
-              </div>
-            </Link>
-          ))}
-        </div>
-      </section>
     </div>
   );
 }
